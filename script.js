b63267a9faedb0d1bc4237f93e20c9a3b1b3c43e
TechMartial <sravanth7762@gmail.com>
2024-06-21 00:32:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function blowCandles() {
   });
   const surprisePage = document.querySelector('.surprise-page');
   surprisePage.style.backgroundColor = '#ff4081';
-  surprisePage.innerHTML += '<p>😘😘 Happy Birthday, Nannaaa! 😘🎉😘/p>';
+  surprisePage.innerHTML += '<p>😘😘 Happy Birthday, Nannaaa! 😘😘/p>';
   const blastEffect = document.createElement('div');
   blastEffect.classList.add('blast-effect');
   surprisePage.appendChild(blastEffect);
